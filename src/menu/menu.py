652e9b621593b8105c82de89a06b8fab77436e0d
Matheus Santos Silva <60097382+matheusssilva991@users.noreply.github.com>
2022-09-10 16:03:49 -0300

    Add files via upload

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -1,25 +1,20 @@
-import datetime
 from src.pessoa.Paciente import Paciente
 from src.pessoa.Especialista import Especialista
 from src.pessoa.Endereco import Endereco
+from src.load_save import load_save
 
 def cadastro_pessoa():
     nome = input(" > Digite o nome: ")
     cpf = input(" > Digite o CPF: ")
     sexo = input(" > Digite o sexo [M/F]: ")
-
-    dia, mes, ano = input(" > Digite a data de nascimento [dd/mm/aa]: ").split("/")
-    data_nascimento = datetime.datetime.strptime(f"{dia} {mes} {ano}", "%d %m %Y")
-
+    data_nascimento = input(" > Digite a data de nascimento [dd/mm/aa]: ")
     endereco = input(" > Digite o endereço: ")
     bairro = input(" > Digite o bairro: ")
     area = input(" > Digite a área [Comercial, Residencial, Rural...]: ")
     endereco = Endereco(endereco, bairro, area)
-
     telefone = input(" > Digite o telefone: ")
 
     return nome, cpf, sexo, data_nascimento, endereco, telefone
-
 
 def menu_cadastro():
     loop_condition = True
@@ -33,20 +28,21 @@ def menu_cadastro():
             case 1:
                 nome, cpf, sexo, data_nascimento, endereco, telefone = cadastro_pessoa()
                 cro = int(input(" > Digite o CRO: "))
-
-                dia, mes, ano = input(" > Digite a data de engresso dd/mm/aa: ").split("/")
-                data_engresso = datetime.datetime.strptime(f"{dia} {mes} {ano}", "%d %m %Y")
-
+                data_engresso = input(" > Digite a data de engresso dd/mm/aa: ")
                 especialidade = input(" > Digite a especialidade: ")
-                
-                especialista = Especialista(nome, cpf, sexo, data_nascimento, endereco, telefone, cro, especialidade, data_engresso)
+                print("\n")
+                especialista = Especialista(nome, cpf, sexo, data_nascimento, endereco, telefone, cro, especialidade,
+                                            data_engresso)
+
+                load_save.save_especialista(especialista)
             case 2:
                 nome, cpf, sexo, data_nascimento, endereco, telefone = cadastro_pessoa()
                 altura = float(input(" > Digite a altura: "))
                 peso = float(input(" > Digite o peso: "))
                 nro_sus = int(input(" > Digite o número do SUS: "))
-                
+                print("\n")
                 paciente = Paciente(nome, cpf, sexo, data_nascimento, telefone, altura, peso, endereco, nro_sus)
+                load_save.save_paciente(paciente)
             case 3:
                 pass
             case 0:
