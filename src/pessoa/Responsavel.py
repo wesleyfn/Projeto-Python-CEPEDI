8a8a189b86578322d90d830a2be057feccdf45f9
igorssant <31260344+igorssant@users.noreply.github.com>
2022-09-10 11:05:46 -0300

    Responsavel e pessoa - ver. 1

---
+++ src/pessoa/Responsavel.py
@@ -0,0 +1,28 @@
+import datetime
+from Pessoa import Pessoa
+
+class Responsavel(Pessoa):
+    def __init__(self, nome_init: str, cpf_init: str, sexo: str, data_nascimento: datetime, telefone: str, grau_parentesco: str):
+        super.__init__(nome_init, cpf_init, sexo, data_nascimento, telefone)
+        self.__grau_parentesco = grau_parentesco
+    # fim __init__
+
+    # metodos Getter abaixo
+
+    @property # metodo getter para grau_parentesco
+    def grau_parentesco(self) -> str:
+        return self.__grau_parentesco
+    # fim grau_parentesco
+
+    # metodos Setter acima
+    # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # metodos Setter abaixo
+
+    @grau_parentesco.setter  # metodo setter para grau_parentesco
+    def grau_parentesco(self, grau_parentesto: str) -> None:
+        self.__grau_parentesco = grau_parentesto
+    # fim grau_parentesco
+
+    # metodos Setter acima
+
+# fim class Responsavel
