a38dc5fbbf01c4d905f24bb98454d350bd326a03
matheusssilva991 <matheus.ssilva991@gmail.com>
2022-09-10 11:30:39 -0300

    "Prontuario getters-setters"

--- src/prontuario/Prontuario.py
+++ src/prontuario/Prontuario.py
@@ -1,3 +1,5 @@
+import datetime
+
 from ..pessoa import Especialista, Paciente, Responsavel
 
 class Prontuario:
@@ -5,3 +7,63 @@ class Prontuario:
         self.__especialista = especialista
         self.__responsavel = responsavel
         self.__paciente = paciente
+        self.__patologia = None
+        self.__procedimento = None
+        self.__data = None
+        self.__consulta = None
+
+    @property
+    def especialista(self) -> Especialista:
+        return self.__especialista
+
+    @property
+    def responsavel(self) -> Responsavel:
+        return self.__responsavel
+
+    @property
+    def paciente(self) -> Paciente:
+        return self.__paciente
+
+    @property
+    def patologia(self) -> list:
+        return self.__patologia
+
+    @property
+    def procedimento(self) -> list[dict]:
+        return self.__procedimento
+
+    @property
+    def data(self) -> datetime:
+        return self.__data
+
+    @property
+    def consulta(self) -> str:
+        return self.__consulta
+
+    @especialista.setter
+    def especialista(self, especialista_setter) -> None:
+        self.__especialista = especialista_setter
+
+    @responsavel.setter
+    def responsavel(self, responsavel_setter) -> None:
+       self.__responsavel = responsavel_setter
+
+    @paciente.setter
+    def paciente(self, paciente_setter) -> None:
+        self.__paciente = paciente_setter
+
+    @patologia.setter
+    def patologia(self, patologia_setter) -> None:
+        self.__patologia = patologia_setter
+
+    @procedimento.setter
+    def procedimento(self, procedimento_setter) -> None:
+        self.__procedimento = procedimento_setter
+
+    @data.setter
+    def data(self, data_setter) -> None:
+        self.__data = data_setter
+
+    @consulta.setter
+    def consulta(self, consulta_setter) -> None:
+        self.__consulta = consulta_setter
