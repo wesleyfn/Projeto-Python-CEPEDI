2d7c74fd88e7bb83a6924d14ee261542c3c9da7b
Matheus Santos Silva <60097382+matheusssilva991@users.noreply.github.com>
2022-09-10 16:04:32 -0300

    Add files via upload

--- src/load_save/load_save.py
+++ src/load_save/load_save.py
@@ -7,24 +7,20 @@ from src.pessoa.Paciente import Paciente
 from src.pessoa.Responsavel import Responsavel
 import os
 
-
 def load_pacientes():
     path = os.path.abspath("../data/pacientes.json")
     with open(path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
-
 
 def load_especialistas():
     path = os.path.abspath("../data/especialistas.json")
     with open(path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
-
 def load_prontuarios():
     path = os.path.abspath("../data/prontuarios.json")
     with open(path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
-
 
 def save_pessoa(pessoa) -> dict:
     new_pessoa = {"nome": pessoa.nome,
@@ -36,6 +32,7 @@ def save_pessoa(pessoa) -> dict:
                                "bairro": pessoa.endereco.bairro,
                                "area": pessoa.endereco.area}
                   }
+    return new_pessoa
 
 def save_paciente(paciente: Paciente) -> None:
     pacientes = load_pacientes()
@@ -61,7 +58,6 @@ def save_especialista(especialista: Especialista):
     path = os.path.abspath("../data/especialistas.json")
     with open(path, 'w', encoding='utf-8') as json_file:
         json.dump(especialistas, json_file, ensure_ascii=False, indent=True)
-
 
 def save_prontuario():
     pass
