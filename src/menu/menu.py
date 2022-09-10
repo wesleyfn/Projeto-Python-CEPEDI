59e31bfdae1e2f8abc2be7d80c08c08c2ab2e09b
wesleyfn <wesleyfnpa@gmail.com>
2022-09-10 16:55:10 -0300

    update menu

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -1,18 +1,41 @@
+from os import execvpe
+from zlib import DEF_BUF_SIZE
 from src.pessoa.Paciente import Paciente
 from src.pessoa.Especialista import Especialista
 from src.pessoa.Endereco import Endereco
 from src.load_save import load_save
 
+def opcao(tipo: str, string: str):
+    x = 0
+    while True:
+        try:
+            match tipo:
+                case 'i':
+                    x = int(input(string))
+                case 'f':
+                    x = float(input(string))
+                case 's':
+                    x = input(string)
+        except:
+            match tipo:
+                case 'i':
+                    print('\n > Digite um número inteiro!\n')
+                case 'f':
+                    print('\n > Digite um número flutuante!\n')
+        else:
+            return x
+    
+
 def cadastro_pessoa():
-    nome = input(" > Digite o nome: ")
-    cpf = input(" > Digite o CPF: ")
-    sexo = input(" > Digite o sexo [M/F]: ")
-    data_nascimento = input(" > Digite a data de nascimento [dd/mm/aa]: ")
-    endereco = input(" > Digite o endereço: ")
-    bairro = input(" > Digite o bairro: ")
-    area = input(" > Digite a área [Comercial, Residencial, Rural...]: ")
+    nome = opcao('s', " > Digite o nome: ")
+    cpf = opcao('s', " > Digite o CPF: ")
+    sexo = opcao('s', " > Digite o sexo [M/F]: ")
+    data_nascimento = opcao('s', " > Digite a data de nascimento [dd/mm/aa]: ")
+    endereco = opcao('s', " > Digite o endereço: ")
+    bairro = opcao('s', " > Digite o bairro: ")
+    area = opcao('s', " > Digite a área [Comercial, Residencial, Rural...]: ")
     endereco = Endereco(endereco, bairro, area)
-    telefone = input(" > Digite o telefone: ")
+    telefone = opcao('s', " > Digite o telefone: ")
 
     return nome, cpf, sexo, data_nascimento, endereco, telefone
 
@@ -21,15 +44,15 @@ def menu_cadastro():
 
     while loop_condition:
         print(" 1. Cadastrar Especialista\n 2. Cadastrar Paciente\n 3. Cadastrar Prontuáio\n 0. Voltar")
-        opcao = int(input(" > Opção: "))
+        op = opcao('i', " > Opção:")
         print("\n")
 
-        match opcao:
+        match op:
             case 1:
                 nome, cpf, sexo, data_nascimento, endereco, telefone = cadastro_pessoa()
-                cro = int(input(" > Digite o CRO: "))
-                data_engresso = input(" > Digite a data de engresso dd/mm/aa: ")
-                especialidade = input(" > Digite a especialidade: ")
+                cro = opcao('i', " > Digite o CRO: ")
+                data_engresso = opcao('s', " > Digite a data de engresso dd/mm/aa: ")
+                especialidade = opcao('s', " > Digite a especialidade: ")
                 print("\n")
                 especialista = Especialista(nome, cpf, sexo, data_nascimento, endereco, telefone, cro, especialidade,
                                             data_engresso)
@@ -37,13 +60,15 @@ def menu_cadastro():
                 load_save.save_especialista(especialista)
             case 2:
                 nome, cpf, sexo, data_nascimento, endereco, telefone = cadastro_pessoa()
-                altura = float(input(" > Digite a altura: "))
-                peso = float(input(" > Digite o peso: "))
-                nro_sus = int(input(" > Digite o número do SUS: "))
+                altura = opcao('f', " > Digite a altura: ")
+                peso = opcao('f', " > Digite o peso: ")
+                nro_sus = opcao('i', " > Digite o número do SUS: ")
                 print("\n")
                 paciente = Paciente(nome, cpf, sexo, data_nascimento, telefone, altura, peso, endereco, nro_sus)
+                
                 load_save.save_paciente(paciente)
             case 3:
+                
                 pass
             case 0:
                 return
@@ -51,19 +76,17 @@ def menu_cadastro():
 
 def main_menu():
     loop_condition = True
-    opcao = -1
-
     while loop_condition:
-        print("1-Cadastros\n"
-              "2-Buscar Especialista\n"
-              "3-Buscar Paciente\n"
-              "4-Buscar Prontuário\n"
-              "5-Importar/Exportar Prontuário\n"
-              "0-Sair\n")
-        opcao = int(input("Digite a opção: "))
+        print(" 1. Cadastros\n"
+              " 2. Buscar Especialista\n"
+              " 3. Buscar Paciente\n"
+              " 4. Buscar Prontuário\n"
+              " 5. Importar/Exportar Prontuário\n"
+              " 0. Sair")
+        op = opcao('i', " > Digite a opção: ")
         print("")
 
-        match opcao:
+        match op:
             case 1:
                 menu_cadastro()
             case 2:
