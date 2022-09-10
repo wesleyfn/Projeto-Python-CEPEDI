315016673a8bcfc7014c990c24c9e6eedf8ae3f9
matheusssilva991 <matheus.ssilva991@gmail.com>
2022-09-10 13:34:18 -0300

    main

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import datetime
+from pessoa.Endereco import Endereco
+from pessoa.Paciente import Paciente
+
+data = datetime.datetime.strptime("10 09 2022", "%d %m %Y")
+endereco_teste = Endereco("Rua de Wesley", "Roca do Povo", "Condominio")
+
+paciente_teste = Paciente("matheus", "cpf", "M", data, "telefone", 1.75, 75.0, endereco_teste, 125)
+
+print(paciente_teste.nome)
